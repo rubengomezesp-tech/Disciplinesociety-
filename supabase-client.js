e7d7dbd21f56bf7ac5ec5683919cf12ef28b3c87
rubengomezesp-tech <rubengomezesp@gmail.com>
2026-04-10 16:51:52 +0200

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -5,8 +5,8 @@
 
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const SUPABASE_URL = 'https://YOUR-PROJECT.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR-ANON-PUBLIC-KEY';
+const SUPABASE_URL = 'https://wqmnvmlvlpxlpucejmtz.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxbW52bWx2bHB4bHB1Y2VqbXR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MTQwNzEsImV4cCI6MjA5MTM5MDA3MX0.I-9bpGaVrK9XvRZuPskUcHfhkKoM7ABikb_gRMui6kk';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
